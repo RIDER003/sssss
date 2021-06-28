9caa0af4d594fb0c65d06394f0ace9b65996d7c1
RIDER003 <84138192+RIDER003@users.noreply.github.com>
2021-06-28 15:49:27 +0545

    Personal Details

---
+++ Assignments/personalDetails.c
@@ -0,0 +1,10 @@
+// Edit this file and push to github.
+
+#include<stdio.h>
+int main(){
+printf("Asian College of Higher Studies\n");
+printf("Siddhartha Acharya\n");
+printf("Texas Internation College\n");
+printf("CGPA: 2.83\n");
+}
+
